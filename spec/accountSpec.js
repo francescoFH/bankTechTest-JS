a8d7e8685271a38ede89cf79a15aac2d4c786e41
francescoFH <francescofacca@gmail.com>
2021-02-24 22:56:34 +0000

    Test: Red - Added feature test that shows current balance updated when you add money twice

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -1,9 +1,17 @@
 describe('Account', () => {
   describe('.deposit', () => {
-    it('results in balance of 200 when passed 200', () => {
+    it('results in balance of 200 when deposited 200', () => {
       const account = new Account();
       expect(account.deposit(100)).toEqual(
         '200.00 deposited. Current balance: 200.00'
+      );
+    });
+
+    it('results in balance of 300 when deposited 200 and then 100', () => {
+      const account = new Account();
+      account.deposit(200);
+      expect(account.deposit(100)).toEqual(
+        '100.00 deposited. Current balance: 300.00'
       );
     });
   });
