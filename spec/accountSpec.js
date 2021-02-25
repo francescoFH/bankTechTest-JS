cb92b7424f5fe4b36d7307c353cdb0d8e670c74f
francescoFH <francescofacca@gmail.com>
2021-02-25 11:01:06 +0000

    Test: Red - Added feature test for deposit method to display decimal number when deposited cents

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -14,5 +14,12 @@ describe('Account', () => {
         '100.00 deposited. Current balance: 300.00'
       );
     });
+
+    it('results in balance of 100.50 when deposited 100.50', () => {
+      const account = new Account();
+      expect(account.deposit(100.50)).toEqual(
+        '100.50 deposited. Current balance: 100.50'
+      );
+    });
   });
 });
