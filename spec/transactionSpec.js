cd85977fe7f467e8762b17e0185725be8f95f8bf
francescoFH <francescofacca@gmail.com>
2021-02-27 19:51:13 +0000

    Test: Red - Added feature test for new class transaction to return a formatted string, with the formatted date in the first column

--- spec/transactionSpec.js
+++ spec/transactionSpec.js
@@ -3,7 +3,7 @@ describe('Transaction', () => {
     it('has the date of the transaction was made in the first column, formatted as dd/mm/yyyy', () => {
       let date = new Date(2021, 2, 27);
       jasmine.clock().install();
-      jasmine.clock.mockDate(date);
+      jasmine.clock().mockDate(date);
       const transaction = new Transaction;
       expect(transaction.display()).toEqual('27/2/2021 || || || ');
       jasmine.clock().uninstall();
