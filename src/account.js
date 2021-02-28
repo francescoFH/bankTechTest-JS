eb57480e3deb9797269fc06127a9254ed12c0fcb
francescoFH <francescofacca@gmail.com>
2021-02-28 01:30:49 +0000

    Deposit and withdraw methods call addTransaction passing in an object with the credit/debit and balance

--- src/account.js
+++ src/account.js
@@ -3,12 +3,13 @@ class Account {
     const STARTING_BALANCE = 0;
     this.balance = STARTING_BALANCE;
     this.transactionClass = transactionClass;
-    this.transactionHistory = []
+    this.transactionHistory = [];
   }
 
   deposit(amount) {
     let credit = toPence(amount)
     this.balance += credit
+    this.addTransaction({ credit: credit, balance: this.balance });
     return `${asPounds(credit)} deposited. Current balance: ${asPounds(this.balance)}`;
   }
 
@@ -16,6 +17,12 @@ class Account {
     let debit = toPence(amount)
     if (debit > this.balance) return 'Insufficient funds'
     this.balance -= debit
+    this.addTransaction({ debit: debit, balance: this.balance });
     return `${asPounds(debit)} withdrawn. Current balance: ${asPounds(this.balance)}`;
+  }
+
+  addTransaction(obj) {
+    const transaction = new this.transactionClass(obj);
+    this.transactionHistory.unshift(transaction);
   }
 }
