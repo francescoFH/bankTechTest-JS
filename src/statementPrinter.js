6f881e0cc7ba2d1764a9b07cd95910f6dc2e8da6
francescoFH <francescofacca@gmail.com>
2021-03-01 16:37:28 +0000

    created statementPrinter class

---
+++ src/statementPrinter.js
@@ -0,0 +1,9 @@
+class StatementPrinter {
+  constructor() {
+    this.STATEMENT_HEADER = 'date || credit || debit || balance \n';
+  }
+  print(transactionArray) {
+    let rows = this.transactionArray.map(transaction => transaction.display());
+    return this.STATEMENT_HEADER + rows.join('\n')
+  }
+}
