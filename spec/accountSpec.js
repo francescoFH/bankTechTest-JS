0167ea5860409d91ff1e6c930b0a0cb86f504335
francescoFH <francescofacca@gmail.com>
2021-03-01 22:36:25 +0000

    Test: Green - Added test for already existing feature of withdraw calls for new Transaction

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -60,5 +60,16 @@ describe('Account', () => {
       account.deposit(100);
       expect(mockObj.Mock).toHaveBeenCalled();
     });
+
+    it("withdraw calls for new Transaction", () => {
+      let mockContainer = {
+        Mock: function () {},
+      };
+      spyOn(mockContainer, "Mock");
+      const account = new Account(mockContainer.Mock);
+      account.deposit(100);
+      account.withdraw(100);
+      expect(mockContainer.Mock).toHaveBeenCalledTimes(2);
+    });
   });
 });
